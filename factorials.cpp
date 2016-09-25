3dc767f493f796f77e560ef2f75eb72bd74942c8
Ian <iff700@hotmail.com>
2016-09-25 13:44:49 -0400

    Find 1-10 Factorial

---
+++ factorials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int factorial=1;
+  for(int n=1; n<=10; n++) { 
+    for(int m=1; m<=n; m++) {
+      factorial*=m;
+    }
+    cout<<"Factorial of "<<n<<" = "<<factorial<<endl;
+     factorial/=factorial;
+  }
+  return 0;
+}
